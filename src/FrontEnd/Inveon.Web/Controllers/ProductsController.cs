9138f0cb3b136c1627498d5c3d4ca05fd03f8e2f
Emirhan Taşdeviren <emirhan.tasdeviren@outlook.com>
2023-11-02 17:21:09 +0300

    Implement products controller

---
+++ src/FrontEnd/Inveon.Web/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Inveon.Web.Models.Dto;
+using Inveon.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Inveon.Web.Controllers;
+
+public class ProductsController : Controller
+{
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        List<ProductDto>? list = null;
+
+        ResponseDto? res = await _productService.GetAllProductsAsync();
+
+        if (res != null && res.IsSuccess)
+        {
+            list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(res.Result)!);
+        }
+        return View(list);
+    }
+}
